a30f8fa682a51ce8f3c3a264935f591d49201786
Utkay Daymaz <utkayar@gmail.com>
2025-11-13 21:17:43 +0300

    Initial commit: Kara colorscheme

---
+++ lua/kara/test_colors.lua
@@ -0,0 +1,159 @@
+-- Color palette extracted from test.lua
+-- This file contains all unique colors used in the theme with semantic names
+
+local colors = {
+	-- Pure colors
+	black = "#000000",
+	white = "#ffffff",
+
+	-- Background shades (dark to light)
+	bg_darkest = "#16181d",
+	bg_darker = "#1a1b26",
+	bg_dark = "#1d222a",
+	bg_base = "#232634",
+	bg_elevated = "#292c3c",
+	bg_alt = "#2a2d3e",
+	bg_surface = "#303446",
+	bg_popup = "#3b3f52",
+	bg_sidebar = "#414559",
+	bg_statusline = "#423b45",
+	bg_float = "#3a4354",
+	bg_menu = "#3b434c",
+	bg_select = "#51576d",
+	bg_visual = "#51576d",
+	bg_cursorline = "#414559",
+	bg_column = "#414559",
+
+	-- Foreground shades (dark to light)
+	fg_dark = "#51576d",
+	fg_gutter = "#626880",
+	fg_comment = "#737994",
+	fg_muted = "#838ba7",
+	fg_inactive = "#949cbb",
+	fg_base = "#c6d0f5",
+	fg_bright = "#f6f8ff",
+
+	-- Blue shades
+	blue = "#8caaee",
+	blue_bright = "#89b4fa",
+	blue_light = "#98b2ef",
+	blue_pale = "#85c1dc",
+	blue_cyan = "#74aacc",
+	blue_sky = "#8094b4",
+	blue_vivid = "#b4befe",
+	blue_accent = "#5e6f9a",
+	blue_dark = "#3d455e",
+	blue_bg = "#363c52",
+	blue_bg_dark = "#384154",
+
+	-- Cyan/Teal shades
+	cyan = "#81c8be",
+	cyan_bright = "#94e2d5",
+	cyan_vivid = "#8afff9",
+	cyan_neon = "#00f1f5",
+	cyan_aqua = "#9affff",
+	cyan_light = "#99d1db",
+	cyan_pale = "#8fc1cc",
+	cyan_muted = "#60968e",
+	cyan_green = "#1abc9c",
+	cyan_bg = "#1a2b32",
+	cyan_bg_light = "#b6f0ff",
+
+	-- Green shades
+	green = "#a6d189",
+	green_bright = "#a6e3a1",
+	green_vivid = "#96f291",
+	green_light = "#abd199",
+	green_lime = "#74cc81",
+	green_sage = "#8eb27c",
+	green_muted = "#5a6d5e",
+	green_dark = "#596a5d",
+	green_bg = "#455052",
+	green_bg_dark = "#536844",
+	green_accent = "#c4ffd3",
+
+	-- Yellow/Gold shades
+	yellow = "#e5c890",
+	yellow_bright = "#f9e2af",
+	yellow_pale = "#d5d685",
+	yellow_lime = "#ffec63",
+	yellow_muted = "#ab966c",
+	yellow_brown = "#726448",
+
+	-- Orange shades
+	orange = "#ef9f76",
+	orange_bright = "#fab387",
+	orange_vivid = "#f79000",
+
+	-- Red/Pink shades
+	red = "#e78284",
+	red_bright = "#f38ba8",
+	red_vivid = "#ff2851",
+	red_dark = "#db4b4b",
+	red_muted = "#ea999c",
+	red_pale = "#eebebe",
+	red_bg = "#514251",
+	red_bg_dark = "#2d202a",
+	red_wine = "#ad6163",
+	red_brown = "#734142",
+	red_mauve = "#6f4f5b",
+	red_rose = "#72505c",
+
+	-- Pink/Magenta shades
+	pink = "#f2d5cf",
+	pink_bright = "#f4b8e4",
+	pink_vivid = "#f70067",
+
+	-- Purple/Mauve shades
+	purple = "#ca9ee6",
+	purple_bright = "#d484ff",
+	purple_vivid = "#c08aff",
+	purple_lavender = "#babbf1",
+	purple_blue = "#cad3f5",
+
+	-- Gray shades
+	gray = "#888a91",
+	gray_light = "#a7a9ae",
+	gray_pale = "#b5bfe2",
+	gray_blue = "#8c94b1",
+	gray_cool = "#949cb7",
+	gray_warm = "#a5adce",
+	gray_dark = "#565a60",
+	gray_darker = "#585b70",
+	gray_slate = "#65696f",
+	gray_steel = "#6f758c",
+	gray_muted = "#cfcfcf",
+	gray_dim = "#8b8b8b",
+
+	-- Special backgrounds
+	diff_add = "#455052",
+	diff_change = "#363c52",
+	diff_delete = "#514251",
+	diff_text = "#4c5778",
+	search_bg = "#506373",
+	search_current = "#e78284",
+	quickfix = "#474d61",
+	separator = "#232634",
+	pmenu_bg = "#414559",
+	pmenu_thumb = "#737994",
+	msg_separator = "#423b45",
+
+	-- Misc colored backgrounds
+	bg_magenta = "#413b4c",
+	bg_purple_dark = "#3d4156",
+	bg_blue_navy = "#393f56",
+	bg_teal = "#384251",
+	bg_gray_cool = "#3b3f52",
+	bg_gray_warm = "#423e4b",
+	bg_steel = "#41424d",
+	bg_slate = "#444d6a",
+	bg_ocean = "#4c5778",
+	bg_cyan_muted = "#729ca4",
+
+	-- Accent colors
+	accent_rose = "#e28e95",
+	accent_coral = "#c27278",
+	accent_teal = "#8aadf4",
+}
+
+return colors
